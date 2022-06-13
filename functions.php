12c9965146b2ea45d3cc14627dcbff2255866fa1
Luan E <luan.erazo@skyrocketlab.space>
2022-06-13 17:29:58 -0500

    agregado la configuración de tailwind

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php 
 function av_assets() {
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700', false, 'all');
     // wp_enqueue_style('estilos', get_stylesheet_directory_uri().'/assets/css/style.css', array('bootstrap'), false, 'all');
-    wp_enqueue_style('estilos', get_stylesheet_directory_uri().'/assets/css/style.css', false, 'all');
+    wp_enqueue_style('estilos', get_stylesheet_directory_uri().'/assets/css/style.css', array('google-fonts'), false, 'all');
     // wp_enqueue_style('blogCss', get_template_directory_uri().'/styles/blog.css',array('estilos'), false, 'all');
 
 }	
