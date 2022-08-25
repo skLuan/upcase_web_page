0f40e4ac370a7bbfa26608b6e43fe776a5dfc3df
Luan E <luan.erazo@skyrocketlab.space>
2022-08-25 15:09:02 -0500

    cambiado el footer y agregado el header 3 porto

--- wp-content/themes/porto-child-upcase/footer/footer-bottom.php
+++ wp-content/themes/porto-child-upcase/footer/footer-bottom.php
@@ -12,8 +12,8 @@ if ( ( ! empty( $porto_settings['footer-logo'] ) && $porto_settings['footer-logo
 			// show logo
 			if ( $porto_settings['footer-logo'] && $porto_settings['footer-logo']['url'] ) :
 				?>
-				<span class="logo">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> - <?php bloginfo( 'description' ); ?>">
+				<span class="d-flex logo">
+					<a class="mx-auto mx-lg-0" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> - <?php bloginfo( 'description' ); ?>">
 						<?php echo '<img class="img-responsive" src="' . esc_url( str_replace( array( 'http:', 'https:' ), '', $porto_settings['footer-logo']['url'] ) ) . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" />'; ?>
 					</a>
 				</span>
@@ -29,12 +29,12 @@ if ( ( ! empty( $porto_settings['footer-logo'] ) && $porto_settings['footer-logo
 		<?php endif; ?>
 
 		<?php if ( ( $porto_settings['footer-payments'] && $porto_settings['footer-payments-image'] && $porto_settings['footer-payments-image']['url'] ) || 'center' == $porto_settings['footer-copyright-pos'] ) : ?>
-			<div class="<?php echo 'center' == $porto_settings['footer-copyright-pos'] || 'right' == $porto_settings['footer-copyright-pos'] || ( 'left' == $porto_settings['footer-copyright-pos'] && is_registered_sidebar( 'footer-bottom' ) && is_active_sidebar( 'footer-bottom' ) ) ? 'footer-center' : 'footer-right'; ?>">
+			<div class="d-flex flex-column d-lg-block <?php echo 'center' == $porto_settings['footer-copyright-pos'] || 'right' == $porto_settings['footer-copyright-pos'] || ( 'left' == $porto_settings['footer-copyright-pos'] && is_registered_sidebar( 'footer-bottom' ) && is_active_sidebar( 'footer-bottom' ) ) ? 'footer-center' : 'footer-right'; ?>">
 				<?php if ( $porto_settings['footer-payments'] && $porto_settings['footer-payments-image'] && $porto_settings['footer-payments-image']['url'] ) : ?>
 					<?php if ( $porto_settings['footer-payments-link'] ) : ?>
 					<a href="<?php echo esc_url( $porto_settings['footer-payments-link'] ); ?>">
 					<?php endif; ?>
-						<ul>
+						<ul class="px-4">
 						<img class="img-responsive footer-payment-img" src="<?php echo esc_url( str_replace( array( 'http:', 'https:' ), '', $porto_settings['footer-payments-image']['url'] ) ); ?>" alt="<?php echo esc_attr( $porto_settings['footer-payments-image-alt'] ); ?>" />
 						</ul>
 					<?php if ( $porto_settings['footer-payments-link'] ) : ?>
@@ -45,7 +45,7 @@ if ( ( ! empty( $porto_settings['footer-logo'] ) && $porto_settings['footer-logo
 				<?php endif; ?>
 				<?php
 				if ( 'center' == $porto_settings['footer-copyright-pos'] ) {
-					echo '<span class="footer-copyright">' . wp_kses_post( $porto_settings['footer-copyright'] ) . '</span>';
+					echo '<span class="mx-auto mx-lg-0 footer-copyright">' . wp_kses_post( $porto_settings['footer-copyright'] ) . '</span>';
 					dynamic_sidebar( 'footer-bottom' );
 				}
 				?>
